b623900e3324ad68c21f94bd996299fd657e273c
Tyler Ryden <tryden11@gmail.com>
2022-05-02 16:59:27 -0500

    Setup MainActivity intent to InputActivity

--- app/src/main/java/com/tryden12/caddyapp/MainActivity.kt
+++ app/src/main/java/com/tryden12/caddyapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.tryden12.caddyapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.tryden12.caddyapp.databinding.ActivityMainBinding
@@ -12,5 +13,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        binding.buttonGetStarted.setOnClickListener {
+            val intent = Intent(this, InputActivity::class.java)
+            startActivity(intent)
+        }
     }
 }

08f30a5db4a33fb355e794000462ae8b233f7a9b
Tyler Ryden <tryden11@gmail.com>
2022-05-02 15:15:05 -0500

    built CreateUser & changed the table column info in User class

--- app/src/main/java/com/tryden12/caddyapp/database/User.kt
+++ app/src/main/java/com/tryden12/caddyapp/database/User.kt
@@ -8,11 +8,11 @@ import androidx.room.PrimaryKey
 @Entity
 class User(
     @PrimaryKey(autoGenerate = true) val userID : Long,
-    @ColumnInfo() val name : String,
     @ColumnInfo() val email : String,
+    @ColumnInfo() val password : String,
     @ColumnInfo() val phoneNumber : String,
 ) {
     override fun toString(): String {
-        return "$name $email $phoneNumber $userID"
+        return "$email $password $phoneNumber $userID"
     }
 }

7690828664d0b9db97c3d1570a6ba98511e35fb2
Tyler Ryden <tryden11@gmail.com>
2022-05-03 08:50:06 -0500

    added private variables and addUser method

--- app/src/main/java/com/tryden12/caddyapp/CreateUserActivity.kt
+++ app/src/main/java/com/tryden12/caddyapp/CreateUserActivity.kt
@@ -3,11 +3,17 @@ package com.tryden12.caddyapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tryden12.caddyapp.database.User
 import com.tryden12.caddyapp.databinding.ActivityCreateUserBinding
 
 class CreateUserActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityCreateUserBinding
+    private var email : String = ""
+    private var password : String = ""
+    private var phoneNumber : String = ""
+
+    private val users = mutableListOf<User>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,5 +24,9 @@ class CreateUserActivity : AppCompatActivity() {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun addUser() {
+
     }
 }

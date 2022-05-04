41f1681f274fe6cf5b9af01378d82ce9aa0d609b
Tyler Ryden <tryden11@gmail.com>
2022-05-04 09:35:20 -0500

    setup listeners, variables, and methods for calculating the right club

--- app/src/main/java/com/tryden12/caddyapp/InputActivity.kt
+++ app/src/main/java/com/tryden12/caddyapp/InputActivity.kt
@@ -3,15 +3,65 @@ package com.tryden12.caddyapp
 import android.os.Binder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.RadioGroup
 import com.tryden12.caddyapp.databinding.ActivityInputBinding
 
-class InputActivity : AppCompatActivity() {
+class InputActivity : AppCompatActivity(), RadioGroup.OnCheckedChangeListener {
 
     private lateinit var binding: ActivityInputBinding
+
+    private var clubDifferential = 0
+
+    private var yardageDifferential = 0
+    private var lieDifferential = 0
+    private var windDifferential = 0
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityInputBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.buttonFindClub.setOnClickListener { findTheClub() }
+        binding.lieTypeRadioGroup.setOnCheckedChangeListener(this)
+    }
+
+
+
+    override fun onCheckedChanged(radioGroup: RadioGroup?, buttonId: Int) {
+        when (buttonId) {
+            /*********** Lie Type ****************/
+            R.id.flat_lie_radioButton -> {
+                lieDifferential = 0
+            }
+            R.id.uphill_lie_radioButton -> {
+                lieDifferential = 1
+            }
+            R.id.downhill_lie_radioButton -> {
+                lieDifferential = -1
+            }
+            R.id.left_lie_radioButton -> {
+                lieDifferential = 1
+            }
+            R.id.right_lie_radioButton -> {
+                lieDifferential = 1
+            }
+            /*********** Wind Direction ****************/
+            R.id.front_wind_radioButton -> {
+                windDifferential = 1
+            } R.id.back_wind_radioButton -> {
+                windDifferential = -1
+            } R.id.left_wind_radioButton -> {
+                windDifferential = 0
+            } R.id.right_wind_radioButton -> {
+                windDifferential = 0
+            }
+        }
+    }
+
+    private fun findTheClub() {
+        var yardage = binding.editTextYardageToHole.text.toString().trim().toInt()
     }
 }
